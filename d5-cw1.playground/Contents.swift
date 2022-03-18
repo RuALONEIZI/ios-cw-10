335a0ffd8f7c1b952634df77cf186915b7b0c996
RuALONEIZI <100702655+RuALONEIZI@users.noreply.github.com>
2022-03-18 17:04:18 +0300

    done

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let family = ["ruaa", "nawaf", "raghad" , "hmoud" , "mohsen" , "dawood" ]
+print(family.count)
+
+
+var evenNumbers = [2,4,6,8,10,12]
+evenNumbers.remove(at: 2)
+
+print(evenNumbers)
+
+evenNumbers += [16,18,20,22]
+print(evenNumbers)
+
+var money = [5.00, 10.00, 15.00]
+money.randomElement()
+
+
+money.removeAll()
+
+var numbers = [1,2,3,4,5]
+
+
+for i in numbers{
+    print(i)
+}
+
+var append = [6,7,8,9,10]
+
+for i in append{
+    print(i)
+}
